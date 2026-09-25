20017f2e511d859caf9169eca6cb2c2f143d2742
Suhana Parvin T S <suhanaparvints@gmail.com>
2026-09-25 13:24:34 +0530

    Time: 1 ms (87.48%), Space: 55.9 MB (60.09%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+     if (n === 0) return true;
+
+    let count = 0;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (
+            flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
+            count++;
+
+            if (count >= n) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
